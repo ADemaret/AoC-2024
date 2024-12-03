e0f6a580b4c3a65b84a04c7a51d6d6e9ca194c88
audrey <audrey.demaret@skynet.be>
2024-12-03 09:44:51 +0100

    day 3 - no regex

---
+++ src/day03_part1.rs
@@ -0,0 +1,73 @@
+use std::time::Instant;
+
+// personal functions
+//use crate::utils::grid2d;
+//use crate::utils::pause;
+//use crate::utils::math;
+
+pub fn main() {
+    println!("-- Advent of Code - Day 03 - Part 1 --");
+    let now = Instant::now();
+
+    let input = include_str!("../assets/day03_input_demo1.txt");
+    // let input = include_str!("../assets/day03_input.txt");
+
+    if let Some(answer) = get_answer(input) {
+        println!("The answer is : {}", answer);
+    } else {
+        println!("No answer found");
+    }
+    let elapsed1 = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed1);
+}
+
+//
+
+fn get_answer(input: &str) -> Option<u32> {
+    Some(
+        input
+            .lines()
+            .map(|line| {
+                let muls = line.split("mul").collect::<Vec<_>>();
+                let mut result = 0;
+                for a_mul in muls {
+                    if a_mul.starts_with('(') {
+                        let a_mul2 = a_mul
+                            .strip_prefix('(')
+                            .unwrap()
+                            .split(')')
+                            .map(|a_mul3| {
+                                let (x, y) = a_mul3.split_once(",").unwrap_or_default();
+                                let x2 = x.parse::<u32>().unwrap_or(0);
+                                let y2 = y.parse::<u32>().unwrap_or(0);
+                                // println!("{},{} => {},{}", x, y, x2, y2);
+                                x2 * y2
+                            })
+                            .collect::<Vec<_>>();
+                        result += a_mul2[0]
+                    } else {
+                        result += 0
+                    }
+                }
+                result
+            })
+            .sum(),
+    )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_total() {
+        assert_eq!(
+            get_answer(include_str!("../assets/day03_input_demo1.txt")),
+            Some(161)
+        );
+        assert_eq!(
+            get_answer(include_str!("../assets/day03_input.txt")),
+            Some(175615763)
+        );
+    }
+}
