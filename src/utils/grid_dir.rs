01291ef54a288255adbcdb34e8659f9788926f2f
audrey <audrey.demaret@skynet.be>
2024-12-06 18:18:19 +0100

    GridDir

--- src/utils/grid_dir.rs
+++ src/utils/grid_dir.rs
@@ -1,66 +1,61 @@
 use crate::utils::grid2d::Grid2D;
 
 #[derive(Clone,Copy,PartialEq,Eq)]
-pub enum Direction {
+pub enum GridDir {
     Up,
     Down,
     Left,
     Right,
 }
 
-#[derive(Clone)]
-pub struct GridDir {
-    pub current: Direction,
-}
-
 impl GridDir {
     pub fn new(dir:&str) -> GridDir {
         match dir {
-            "Up" => GridDir { current:Direction::Up},
-            "Down" => GridDir { current:Direction::Down},
-            "Left" => GridDir { current:Direction::Left},
-            "Right" => GridDir { current:Direction::Right},
-            _ => panic!("Invalid direction")
+            "Up" => {GridDir::Up},
+            "Down" => {GridDir::Down},
+            "Left" => {GridDir::Left},
+            "Right" => {GridDir::Right},
+            _ => panic!("Invalid GridDir")
         }
     }
     pub fn invert_dir(&mut self) {
-        match &self.current {
-            Direction::Up => self.current = Direction::Down,
-            Direction::Down => self.current = Direction::Up,
-            Direction::Left => self.current = Direction::Right,
-            Direction::Right => self.current = Direction::Left,
+        match &self {
+            GridDir::Up => *self = GridDir::Down,
+            GridDir::Down => *self = GridDir::Up,
+            GridDir::Left => *self = GridDir::Right,
+            GridDir::Right => *self = GridDir::Left,
         }
     }
 
     pub fn move_right(&mut self) {
-        match &self.current {
-            Direction::Up => self.current = Direction::Right,
-            Direction::Right => self.current = Direction::Down,
-            Direction::Down => self.current = Direction::Left,
-            Direction::Left => self.current = Direction::Up,
+        match &self {
+            GridDir::Up => *self = GridDir::Right,
+            GridDir::Right => *self = GridDir::Down,
+            GridDir::Down => *self = GridDir::Left,
+            GridDir::Left => *self = GridDir::Up,
         }
     }
 
     pub fn move_point(&self, grid: &Grid2D, pt: (usize, usize)) -> Option<(usize, usize)> {
         // test if move is out of bound
-        match &self.current {
-            Direction::Up => {
+        match &self {
+            GridDir::Up => {
                 if pt.0 == 0 {
                     None
                 } else {Some((pt.0-1,pt.1))}
             }
-            Direction::Down => {
+            GridDir::Down => {
                 if pt.0 == grid.max_l - 1 {
                     None
                 } else {Some((pt.0+1,pt.1))}
             }
-            Direction::Left => {
+            GridDir::Left => {
                 if pt.1 == 0 {
                     None
                 } else {Some((pt.0,pt.1-1))}
             }
             
-            Direction::Right => {
+            GridDir::Right => {
                 if pt.1 == grid.max_c - 1 {
                     None
                 } else {Some((pt.0,pt.1+1))}
