dda99755ed544d4f370efe3c42a2bab88777971b
audrey <audrey.demaret@skynet.be>
2024-12-07 15:29:15 +0100

    recursive function

--- src/day07_part2.rs
+++ src/day07_part2.rs
@@ -23,6 +23,7 @@ fn get_answer(input: &str) -> u64 {
     input
         .lines()
         .map(|line| {
+            // println!("Equ {}", line);
             let parts = line.split_once(": ").unwrap();
             let sol = parts.0.parse::<u64>().unwrap();
             let params = parts
@@ -31,6 +32,7 @@ fn get_answer(input: &str) -> u64 {
                 .map(|x| x.parse::<u64>().unwrap())
                 .collect::<Vec<_>>();
             if sol_exists(sol, params) {
+                // println!("adding {}", sol);
                 sol
             } else {
                 0
@@ -40,85 +42,35 @@ fn get_answer(input: &str) -> u64 {
 }
 
 fn sol_exists(sol: u64, params: Vec<u64>) -> bool {
-    let mut possibilities: Vec<u64> = Vec::new();
-
-    possibilities.push(next_operation(sol, params.clone(), 0, 0, "add"));
-    possibilities.push(next_operation(sol, params.clone(), 0, 0, "mult"));
-    possibilities.push(next_operation(sol, params.clone(), 0, 0, "con"));
-    // println!("equ {:?} => first pair gives {:?}",equ,possibilities);
-
-    for index in 2..params.len() {
-        let mut to_add = Vec::new();
-        for poss in possibilities.iter_mut() {
-            if *poss != 0 {
-                to_add.push(next_operation(sol, params.clone(), poss.clone(), index, "add"));
-                to_add.push(next_operation(sol, params.clone(), poss.clone(), index, "con"));
-                *poss = next_operation(sol, params.clone(), poss.clone(), index, "mult");
-            }
-        }
-        for ta in to_add {
-            possibilities.push(ta.clone());
-        }
-        // println!("equ {:?} => index {} gives {:?}",equ,index, possibilities);
+    // println!("- {}, {:?}", sol, params);
+    if params.len() == 1 {
+        // if params[0] == sol {
+        //     println!("ok : {}", params[0]);
+        // } else {
+        //     println!("no : {}", params[0]);
+        // }
+        return params[0] == sol;
     }
-    // println!("equ {:?} => done {:?}",equ,possibilities);
-    if possibilities.contains(&sol) {
+
+    // we start by last param to filter at each step
+    let (last, params) = params.split_last().unwrap();
+
+    // add
+    if *last <= sol && sol_exists(sol - last, params.to_vec()) {
         return true;
     }
+    // mult
+    if sol % *last == 0 && sol_exists(sol / last, params.to_vec()) {
+        return true;
+    }
+    // con
+    // ex : 486:6*8||6 => modulo = 10 => 486%10 = 6 && sol_exists(48,[6,8]))
+    let modulo = 10u64.pow((*last).ilog10() + 1);
+    if sol % modulo == *last && sol_exists(/*(*/ sol/modulo /*) as u64*/, params.to_vec()) {
+        return true;
+    }
+
     false
-}
-
-fn next_operation(sol: u64, param: Vec<u64>, temp_sol: u64, index: usize, sign: &str) -> u64 {
-    let poss: u64;
-    match sign {
-        "add" => {
-            if index == 0 {
-                // print!(" {}+{}",param[0],param[1]);
-                poss = param[0] + param[1];
-            } else {
-                // print!(" {}+{}",temp_sol,param[index]);
-                poss = temp_sol + param[index];
-            }
-        }
-        "mult" => {
-            if index == 0 {
-                // print!(" {}x{}",param[0],param[1]);
-                poss = param[0] * param[1];
-            } else {
-                if index == param.len() - 1 {
-                    if sol % param[index] != 0 {
-                        //println!("{}%{}={} refused",sol,param[index],sol%param[index]);
-                        return 0;
-                    }
-                }
-                // print!(" {}x{}",temp_sol,param[index]);
-                poss = temp_sol * param[index];
-            }
-        }
-        "con" => {
-            if index == 0 {
-                // print!(" {}x{}",param[0],param[1]);
-                poss = my_concat(param[0], param[1]);
-            } else {
-                // print!(" {}x{}",temp_sol,param[index]);
-                poss = my_concat(temp_sol, param[index]);
-            }
-        }
-        _ => panic!(),
-    }
-    if poss > sol {
-        // println!(" = {} too high =>0",poss);
-        return 0;
-    }
-    // println!(" = {}",poss);
-    poss
-}
-
-fn my_concat(a: u64, b: u64) -> u64 {
-    // length of b
-    let width = b.ilog10()+1;
-    // println!("concat of {},{} is {}",a,b,a * 10u64.pow(width as u32)+b);
-    a * 10u64.pow(width) + b
 }
 
 #[cfg(test)]
